71e7e38f9dabb4dd91e57bf5bb6ee3d979e92933
Nick DeJesus <ndejesus@sapient.com>
2016-05-06 10:49:49 +0100

    kick off

---
+++ app/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Table from './Table.jsx';
+
+export default class App extends React.Component {
+  render() {
+    return <Table />;
+  }
+}
---
+++ app/components/Table.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default class Table extends React.Component{
+	render() {
+		return <h1>Tekken Chicken</h1>
+	}
+ }
