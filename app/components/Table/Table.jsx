8293a504d8065cfc1dcea1cdfe2e985afe1068b0
Nick DeJesus <ndejesus@sapient.com>
2016-05-09 13:40:23 +0100

    css stuff

---
+++ app/components/Table/Table.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default class Table extends React.Component{
+	render() {
+		return <h2 id="test">Table</h2>
+	}
+ }
--- build/bundle.js
+++ build/bundle.js
@@ -20497,7 +20497,7 @@
 	  _createClass(App, [{
 	    key: 'render',
 	    value: function render() {
-	      return _react2.default.createElement(Table, null);
+	      return _react2.default.createElement(_Table2.default, null);
 	    }
 	  }]);
 
@@ -20513,8 +20513,11 @@
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+		value: true
 	});
+	exports.default = undefined;
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	var _react = __webpack_require__(5);
 
@@ -20522,13 +20525,36 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	exports.default = function () {
-	  return _react2.default.createElement(
-	    'h1',
-	    null,
-	    'Tekken Chicken'
-	  );
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Table = function (_React$Component) {
+		_inherits(Table, _React$Component);
+
+		function Table() {
+			_classCallCheck(this, Table);
+
+			return _possibleConstructorReturn(this, Object.getPrototypeOf(Table).apply(this, arguments));
+		}
+
+		_createClass(Table, [{
+			key: 'render',
+			value: function render() {
+				return _react2.default.createElement(
+					'h1',
+					null,
+					'Tekken Chicken'
+				);
+			}
+		}]);
+
+		return Table;
+	}(_react2.default.Component);
+
+	exports.default = Table;
 
 /***/ }
 /******/ ]);
