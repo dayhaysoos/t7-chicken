de900dafb6aa246a9033dead2510ba0d402894e5
Nick DeJesus <ndejesus@sapient.com>
2016-05-31 11:53:55 +0200

    Character drop down list. Working out of Master branch like a boss

--- app/components/FrameData/FrameData.jsx
+++ app/components/FrameData/FrameData.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
 
-export default class FrameData extends React.Component{
+class CharacterList extends React.Component {
+	renderCharNames(charData) {
+		console.log(charData);
+		let charNames = [];
+		for(var key in charData) {
+			if(charData.hasOwnProperty(key)) {
+				this.charData = charData;
+				charNames.push(<option className="character-name">
+					{this.charData[key].metadata.character}
+					</option>);
+			}
+		}
+		return charNames;
+	}
 	render() {
+		return ( 
+			<select className="char-list">
+				{this.renderCharNames(this.props.charData)}
+			</select>
+			);
+	}
+}
 
+export default class FrameData extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	
+
+	componentWillMount() {
 		function requireAll(requireContext) {
 			return requireContext.keys().map(requireContext);
 		}
 
-		var data = requireAll(require.context('../data/t7', false, /\.json$/));
-		console.log(data[0]);
+		let data = requireAll(require.context('../data/t7', false, /\.json$/));
 
-		return <h2 id="test">Frame Data</h2>
+		this.state = {
+			data
+		}
+	}
+
+	render() {
+		return( 
+			<div className="frame-data-container container text-center">
+			<h2>Frame Data</h2>
+				<CharacterList charData={this.state.data}></CharacterList>
+			</div>
+		)
 	}
  }
--- app/components/Main.js
+++ app/components/Main.js
@@ -5,7 +5,7 @@ export default class Main extends React.Component {
   render() {
 
     return(
-      <div>
+      <div className="container">
         <Navbar />
         {React.cloneElement(this.props.children, {...this.props})}
       </div>
