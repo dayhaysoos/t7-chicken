67c4a433bb1534f3948c2be2dffdaedf70d5582d
Scott Jones <scottdj92@gmail.com>
2016-11-06 12:29:25 -0500

    update main menu to leverage foundation

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router';
 export default class Navbar extends React.Component {
 	render(){
 		return(
-		<nav className="navbar">
-			<div className="container-fluid">
-			<div className="navbar-header"></div>
-				<div className="navbar-collapse collaspse">
-					<ul className="nav navbar-nav">
-						<li><a href="#">TC</a></li>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/frame-data">Frame Data</Link></li>
-					</ul>
-					<ul className="nav navbar-nav navbar-right">
-					<li><Link to="/login">Login</Link></li>
-					<li><Link to="/register">Register</Link></li>
-					</ul> 
+			<nav className="navbar">
+				<div className="row">
+					<div className='small-6 columns'>
+						<ul className="menu">
+							<li><a href="#">TC</a></li>
+							<li><Link to="/">Home</Link></li>
+							<li><Link to="/frame-data">Frame Data</Link></li>
+						</ul>
+					</div>
+					<div className='small-5 columns'>
+						<ul className="menu align-right">
+							<li><Link to="/login">Login</Link></li>
+							<li><Link to="/register">Register</Link></li>
+						</ul>
+					</div>
 				</div>
-			</div>
-		</nav>
-
-			);
+			</nav>
+		);
 	}
 }
