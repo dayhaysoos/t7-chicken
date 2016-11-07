d3160dc1f38b6f7bc4b4ab25f54e9e61144aa942
Nick DeJesus <ndejesus@sapient.com>
2016-11-07 12:31:01 -0500

    instead of having the log in page be a different entity, I made it part of the nav bar

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 export default class Login extends React.Component {
 
-	render() {
-		return (
-		<div className="login-container row">
-			<div className='small-8 columns centered'>
-				<form className="form-signin">
-					<h2 className="form-signin-heading">Please sign in</h2>
-					<label htmlFor="inputEmail" className="sr-only">Email address</label>
-					<input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autofocus=""></input>
-					<label htmlFor="inputPassword" className="sr-only">Password</label>
-					<input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""></input>
-
-		        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		      	</form>
-		      	<h2>Or</h2>
-		      	<Link to="/register">Register</Link>
-			</div>
-	  </div>
-		)
+	handleClick(event) {
+		console.log(this.props);
+		const username = this.refs.username;
+		const password = this.refs.password;
+		const creds = { username: username.value.trim(), password: password.value.trim() }
+		this.props.onLoginClick(creds);
 	}
+
+render() {
+    const { errorMessage } = this.props
+
+    return (
+      <div>
+        <input type='text' ref='username' placeholder='Username'/>
+        <input type='password' ref='password' placeholder='Password'/>
+        <button onClick={(event) => this.handleClick(event)} type="submit" className="btn btn-primary">
+          Login
+        </button>
+
+        {errorMessage &&
+          <p style={errorMessage}></p>
+        }
+      </div>
+    )
+  }
 }
